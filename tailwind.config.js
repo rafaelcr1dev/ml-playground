9dfed86e4444e58ba05f6c3d2a27a1e8a5a8cc0b
rafaelcr123 <rafaelcruz1@hotmail.com>
2021-07-28 20:33:30 -0300

    refactor: change darkMode of  tailwind.config.js to class and path of purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  purge: [
+    './src/pages/*.{js,ts,jsx,tsx}', 
+    './src/components/**/*.{js,ts,jsx,tsx}',
+  ],
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {},
   },
